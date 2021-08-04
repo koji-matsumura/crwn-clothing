b72438a93ab123bc8f823199372e8bd04cd2bff1
koji-matsumura <k.m.creation226@gmail.com>
2021-08-04 13:30:58 +0900

    Updating app with a shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 //import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component.jsx';
 
 // In case of "<Route exact...",
 // a page will be shown if a URI user specified matches a path.
@@ -20,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
