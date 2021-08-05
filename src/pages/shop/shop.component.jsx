0ae1ba727bc7548394f9823a143c8e963a6019fa
koji-matsumura <k.m.creation226@gmail.com>
2021-08-05 22:28:19 +0900

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,62 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+//import SHOP_DATA from './shop.data';
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+// import { createStructuredSelector } from 'reselect';
+// import { connect } from 'react-redux';
+// import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import { Route } from 'react-router-dom';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview
-            key={id}
-            {...otherCollectionProps}
-          ></CollectionPreview>
-        ))}
-      </div>
-    );
-  }
-}
+// Router will store an actual uri in a variable named categoryId.
+const ShopPage = ({ match }) => {
+  //console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
+
+// const ShopPage = ({ collections }) => (
+//   <div className="shop-page">
+//     {collections.map(({ id, ...otherCollectionProps }) => (
+//       <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
+//     ))}
+//   </div>
+// );
+
+// const mapStateToProps = createStructuredSelector({
+//   collections: selectCollections,
+// });
+
+// export default connect(mapStateToProps)(ShopPage);
+
+// class ShopPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       collections: SHOP_DATA,
+//     };
+//   }
+
+//   render() {
+//     const { collections } = this.state;
+//     return (
+//       <div className="shop-page">
+//         {collections.map(({ id, ...otherCollectionProps }) => (
+//           <CollectionPreview
+//             key={id}
+//             {...otherCollectionProps}
+//           ></CollectionPreview>
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// export default ShopPage;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,29 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//import sessionStorage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], // Describe only something you want to persist
+};
+
+// export default combineReducers({
+//   user: userReducer,
+//   cart: cartReducer,
+// });
+
+const rootRecuder = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootRecuder);
